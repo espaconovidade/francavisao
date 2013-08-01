df913fc8e970414c991c1e45cc30cca9c298fb66
Johnny Vaz <johnnyvaz@espaconovidade.com.br>
2013-08-01 16:38:50 -0300

    adicionando arquivos no repositório
    adicionando arquivos no repositório

---
+++ site/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-03-01 05:06:09 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-03-01T05:06:09+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-03-01T05:06:29+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
